9d72425797b519ab66fa9234357d390c07d2665b
rexsaurus <77756242+rexsaurus@users.noreply.github.com>
2023-12-04 21:16:07 +0000

    adding scene detail

--- src/components/MovieGrid.vue
+++ src/components/MovieGrid.vue
@@ -1,24 +1,34 @@
 <template>
-  <div class="movie-grid">
-    <MovieSquare
-      v-for="(movie, index) in movies"
-      :key="index"
-      :isMint="movie.isMint"
-      :movieSegment="movie.segment"
-    />
+  <div class="movie-grid-container">
+    <div class="movie-grid">
+      <MovieSquare
+        v-for="(movie, index) in movies"
+        :key="index"
+        :isMint="movie.isMint"
+        :movieSegment="movie.segment"
+        @click="openSceneDetail(movie)"
+      />
+    </div>
+    <div class="scene-detail-popup" v-if="isSceneDetailVisible">
+      <SceneDetail
+        :scene="selectedScene"
+        @close="isSceneDetailVisible = false"
+      />
+    </div>
   </div>
 </template>
 
 <script>
 import MovieSquare from './MovieSquare.vue';
+import SceneDetail from './SceneDetail.vue';
 
 export default {
   components: {
-    MovieSquare
+    MovieSquare,
+    SceneDetail
   },
   data() {
     return {
-      // Example data structure for movies, you would fetch or calculate this data as required
       movies: [
         { isMint: true, segment: 'segment1.mp4' },
         { isMint: false, segment: 'segment2.mp4' },
@@ -37,18 +47,42 @@ export default {
         { isMint: true, segment: 'segment1.mp4' },
         { isMint: false, segment: 'segment2.mp4' }
         // ...more movie objects
-      ]
+      ],
+      isSceneDetailVisible: false,
+      selectedScene: null,
     };
+  },
+  methods: {
+    openSceneDetail(movie) {
+      this.selectedScene = movie;
+      this.isSceneDetailVisible = true;
+    }
   }
 };
 </script>
 
 <style scoped>
+  .movie-grid-container {
+    position: relative; /* This makes it a context for absolutely positioned children */
+  }
+
   .movie-grid {
     display: grid;
-    grid-template-columns: repeat(4, 1fr); /* 4 columns */
-    gap: 15px; /* 15px gap between items */
-    padding: 15px; /* Optional: padding around the grid */
-    width: 100%; /* Full width */
+    grid-template-columns: repeat(4, 1fr);
+    gap: 15px;
+    padding: 15px;
+    width: 100%;
+  }
+
+  .scene-detail-popup {
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-color: rgba(0, 0, 0, 0.5); /* Semi-transparent background */
   }
 </style>
