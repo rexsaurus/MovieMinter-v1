6e0d021836b1873294c97d0b6a99579e82c40437
rexsaurus <77756242+rexsaurus@users.noreply.github.com>
2023-12-04 22:45:38 +0000

    fixing the transparency of mintable items

--- src/components/MovieSquare.vue
+++ src/components/MovieSquare.vue
@@ -7,6 +7,9 @@
   >
     <!-- Image or video content goes here -->
     <img :src="imagePath" class="scene-thumbnail" alt="Movie Scene" />
+    
+    <!-- Conditionally render "MINT" text for isMint = false -->
+    <div v-if="!isMint" class="mint-text">MINT</div>
   </div>
 </template>
 
@@ -41,15 +44,21 @@ export default {
     position: relative;
     width: 100%;
     height: 85px; /* Fixed height */
-
-    /* Remove background-image and background-size properties */
-    /* background-image: url('./assets/film_icon.png'); */
-    /* background-size: 50%; */
-
     border: 1px solid transparent; /* Default border set to transparent */
     cursor: pointer; /* Changes the cursor to a pointer on hover */
     transition: border-color 0.3s ease; /* Smooth transition for border color */
-}
+
+    /* Default background image and opacity */
+    background-image: url('./assets/film_icon.png');
+    background-size: 50%;
+    background-repeat: no-repeat;
+    background-position: center;
+    opacity: 1; /* Default opacity for scenes with isMint = false */
+    text-align: center; /* Center text horizontally */
+    display: flex;
+    align-items: center; /* Center text vertically */
+    justify-content: center;
+  }
 
 /* Add styling for the scene-thumbnail image */
 .scene-thumbnail {
@@ -58,10 +67,28 @@ export default {
   object-fit: cover; /* Maintain aspect ratio and cover the entire container */
 }
 
+/* Apply 30% opacity to scenes with isMint = true */
+.mint {
+  opacity: 0.3;
+}
+
+/* Style for the "MINT" text */
+.mint-text {
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 1.5rem; /* Adjust font size as needed */
+  color: #ffffff; /* Text color */
+  background-color: rgba(0, 0, 0, 0.3); /* Background color with 30% opacity */
+}
+
+/* Add green border on hover */
 .movie-square:hover {
-    color: #42b983; /* Changes text color on hover */
-    border-color: #42b983; /* Changes border color on hover */
-    border-width: 3px; /* Sets border width on hover */
-    border-style: solid; /* Ensures the border style is solid */
+  border: 2px solid #42b983; /* Green border on hover */
 }
 </style>
