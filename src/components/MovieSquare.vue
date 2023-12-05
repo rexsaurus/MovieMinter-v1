0b43e65336ec52fe238905f8b277371616516241
rexsaurus <77756242+rexsaurus@users.noreply.github.com>
2023-12-05 22:05:14 +0000

    text size on grid squares adjustment

--- src/components/MovieSquare.vue
+++ src/components/MovieSquare.vue
@@ -39,60 +39,31 @@ export default {
 </script>
 
 <style scoped>
-.movie-square {
-    background-color: #f0f0f0; /* Example background color */
-    position: relative;
-    width: 100%;
-    height: 120px; /* Fixed height */
-    border: 1px solid transparent; /* Default border set to transparent */
-    cursor: pointer; /* Changes the cursor to a pointer on hover */
-    transition: border-color 0.3s ease; /* Smooth transition for border color */
-
-    /* Default background image and opacity */
-    background-size: 50%;
-    background-repeat: no-repeat;
-    background-position: center;
-    opacity: 1; /* Default opacity for scenes with isMint = false */
-    text-align: center; /* Center text horizontally */
-    display: flex;
-    align-items: center; /* Center text vertically */
-    justify-content: center;
+  .movie-square {
+      background-color: #f0f0f0; /* Example background color */
+      position: relative;
+      width: 100%;
+      height: 120px; /* Fixed height */
+      border: 1px solid transparent; /* Default border set to transparent */
+      cursor: pointer; /* Changes the cursor to a pointer on hover */
+      transition: border-color 0.3s ease; /* Smooth transition for border color */
+      display: flex;
+      align-items: center; /* Center text vertically */
+      justify-content: center;
+      text-align: center; /* Center text horizontally */
   }
 
-/* Add styling for the scene-thumbnail image */
-.scene-thumbnail {
-  width: 100%;
-  height: 100%;
-  object-fit: cover; /* Maintain aspect ratio and cover the entire container */
-}
+  .scene-thumbnail {
+    width: 100%;
+    height: 100%;
+    object-fit: cover; /* Maintain aspect ratio and cover the entire container */
+  }
 
-/* Apply 30% opacity to scenes with isMint = true */
-.mint {
-  opacity: 0.3;
-}
+  .mint {
+    opacity: 0.3;
+  }
 
-/* Style for the "MINT" text */
-.mint-text {
-  position: absolute;
-  top: 0;
-  left: 0;
-  right: 0;
-  bottom: 0;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  font-size: 1.5rem; /* Adjust font size as needed */
-  color: #ffffff; /* Text color */
-  background-color: rgba(0, 0, 0, 0.3); /* Background color with 30% opacity */
-}
-
-/* Add green border on hover */
-.movie-square:hover {
-  border: 2px solid #42b983; /* Green border on hover */
-}
-
-  /* Style for the "unavailable" text */
-  .unavailable-text {
+  .mint-text, .unavailable-text {
     position: absolute;
     top: 0;
     left: 0;
@@ -101,8 +72,39 @@ export default {
     display: flex;
     align-items: center;
     justify-content: center;
-    font-size: 1.5rem; /* Adjust font size as needed */
+    padding: 10px; /* Padding to avoid text touching the edges */
+    word-wrap: break-word; /* Ensures long words do not overflow */
+    overflow-wrap: break-word; /* Alternative to word-wrap */
+    font-size: 2.5vw; /* Starting font size based on viewport width */
     color: #ffffff; /* Text color */
+    text-shadow: 0 0 4px rgba(0,0,0,0.6); /* Text shadow for better readability */
+  }
+
+  .mint-text {
+    background-color: rgba(0, 0, 0, 0.3); /* Background color with 30% opacity */
+  }
+
+  .unavailable-text {
     background-color: rgba(255, 0, 0, 0.3); /* Red background color with 30% opacity */
   }
+
+  /* Media Queries for specific screen sizes */
+  @media (max-width: 600px) {
+    .mint-text, .unavailable-text {
+      font-size: 4vw; /* Larger font size for smaller screens */
+    }
+  }
+
+  @media (min-width: 601px) {
+    .mint-text, .unavailable-text {
+      font-size: 2.5vw; /* Adjust font size for medium screens */
+    }
+  }
+
+  @media (min-width: 1024px) {
+    .mint-text, .unavailable-text {
+      font-size: 1.25vw; /* Smaller font size for larger screens */
+    }
+  }
+
 </style>
