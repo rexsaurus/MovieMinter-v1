756c39437a57f861fee8faeafd814ce14f14f5e1
rexsaurus <77756242+rexsaurus@users.noreply.github.com>
2023-12-05 02:48:34 +0000

    adding replicate

--- src/components/SceneDetail.vue
+++ src/components/SceneDetail.vue
@@ -11,23 +11,77 @@
         <p>{{ scene.description }}</p>
         <p>Est. Processing time: {{ scene.estProcessingTime }}</p>
       </div>
-      <button class="mint-button" @click="closeForm">Mint</button>
+      <div v-if="videoLoaded" class="scene-video">
+        <button v-if="videoUrl" class="play-button" @click="viewVideo">Play Video</button>
+      </div>
+      <div v-if="isLoading" class="loading-indicator">Processing...</div>
+      <div v-else>
+        <!-- existing code -->
+        <button class="mint-button" @click="mintScene">Mint</button>
+      </div>
     </div>
   </div>
 </template>
 
+
 <script>
+import Replicate from "replicate";
+
 export default {
   props: {
     scene: Object
   },
+  data() {
+    return {
+      isLoading: false,
+      videoUrl: null, // URL of the processed video
+      videoLoaded: false // Indicates if the video is loaded
+    };
+  },
   methods: {
     closeForm() {
       this.$emit('close');
+    },
+    async mintScene() {
+      this.isLoading = true;
+
+      // Set up Replicate API call
+      const replicate = new Replicate({
+        auth: process.env.REPLIT_TOKEN,
+      });
+
+      try {
+        const output = await replicate.run(
+          "stability-ai/stable-video-diffusion:3f0457e4619daac51203dedb472816fd4af51f3149fa7a9e0b5ffcf1b8172438",
+          {
+            input: {
+              input_image: this.scene.imagePath
+              // Add other parameters as required
+            }
+          }
+        );
+
+        // Store the video URL and indicate the video is loaded
+        this.videoUrl = output;
+        this.videoLoaded = true;
+
+      } catch (error) {
+        console.error("Error processing scene:", error);
+      } finally {
+        this.isLoading = false;
+      }
+    },
+    viewVideo() {
+      if (this.videoUrl) {
+        // Open the video in a new tab or modal
+        window.open(this.videoUrl, '_blank');
+      }
     }
   }
 };
 </script>
+
+
 
 <style scoped>
 .scene-detail-container {
@@ -89,6 +143,17 @@ export default {
   margin: 0.5rem 0;
 }
 
+  .play-button {
+    padding: 0.5rem 1rem;
+    background-color: orange; /* Set the background color to orange */
+    color: white;
+    border: none;
+    border-radius: 5px;
+    font-weight: bold;
+    cursor: pointer;
+    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.2);
+  }
+
 .mint-button {
   padding: 0.5rem 1rem;
   margin-top: 1rem; /* Space from the text above */
@@ -103,4 +168,15 @@ export default {
   font-size: 1.25rem;
   box-shadow: 0 2px 4px rgba(0, 0, 0, 0.2); /* Optional: add a shadow for depth */
 }
+
+  .loading-indicator {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    /* add more styles as needed */
+  }
+
+  .play-button {
+    /* Add your styling here */
+  }
 </style>
